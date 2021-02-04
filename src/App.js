31198f921e9ef922270856774245bb8cf1f6d355
Ulises López <ulises050794@gmail.com>
2021-02-03 21:56:28 -0600

    based configuration of props added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Fragment } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import Getter from './elements/getter';
+import Tasks from './elements/tasks'; 
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [zeroTasks, setZeroTasks] = useState(false);
+  // object for new tasks
+  function task(title, done) {
+    this.title = title;
+    // true - donde -- false - pending
+    this.isDone = done;
+  }  
+
+  useEffect(() => { 
+    setTasks([]);
+  }, []);
+
   return (
-    <Fragment> 
-      <Getter />
-    </Fragment>
-  );
+    <> 
+      <Getter tasks={ tasks } setTasks={ setTasks } task={ task } setZeroTasks={ setZeroTasks } />
+      <Tasks todo={ tasks } zeroTasks={ zeroTasks } />
+    </>
+  ); 
+  
 }
 
 export default App;
