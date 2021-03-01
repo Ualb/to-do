290e94730267e2b848f613adc0f0ea8bd6a875bc
Ulises López <ulises050794@gmail.com>
2021-02-28 18:53:57 -0600

    se agregó newlist alert

--- src/commons/alerts.js
+++ src/commons/alerts.js
@@ -4,7 +4,9 @@ const typeAlert = {
     'FAILURE': 'Ha ocurrido un fallo, contáctate con el proveedor!',
     // login
     'WENOTFOUNDYOU': 'No te hemos encontrado.',
-    'PASSWORDDONOTMATCH': 'Has que coincidan las contraseñas.'
+    'PASSWORDDONOTMATCH': 'Has que coincidan las contraseñas.',
+    // Lists
+    'NEWLIST': 'Ya tienes una nueva lista!',
 }
 
 
